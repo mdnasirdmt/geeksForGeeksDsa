59d62d46e80af2ba584f93672fd0a09ff565fc65
Md Nasir uddin <mdnasirdmt@gmail.com>
2023-02-23 08:28:35 +0530

    setup ready for practice

---
+++ GeeksForGeeksDSAProblem/src/dsaQuestions/ReversArray.java
@@ -0,0 +1,21 @@
+package dsaQuestions;
+
+public class ReversArray {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
+/*
+ * Write a program to reverse an array or string
+
+ * Given an array (or string), the task is to reverse the array/string. 
+ * Examples:
+ * 
+ * Input : arr[] = {1, 2, 3} Output : arr[] = {3, 2, 1}
+ * 
+ * Input : arr[] = {4, 5, 1, 2} Output : arr[] = {2, 1, 5, 4}
+ */
